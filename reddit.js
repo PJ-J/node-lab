5c40f50c84db809159fae404a7dc4d6dc63c01c6
PJ Jones <pj.jones4000.com>
2022-03-23 09:57:56 -0500

    formatting

--- reddit.js
+++ reddit.js
@@ -13,26 +13,11 @@ request("https://reddit.com/r/popular.json", (err, res, body) => {
     let obj = {
       title: item.data.title,
       url: item.data.url,
-      author: item.data.author
+      author: item.data.author,
     };
     data.push(obj);
-    
   });
-  fs.appendFileSync(dataPath, JSON.stringify(data), 'utf-8', err => {
-    if(err) console.log(err);
+  fs.appendFileSync(dataPath, JSON.stringify(data), "utf-8", (err) => {
+    if (err) console.log(err);
   });
-  // console.log(arr);
-
-  
-  // fs.appendFile(dataPath,{ 
-  //   encoding: "UTF-8"}, (err,arr) => {
-  //   arr;
-  // });
-  
-  
-
-  // fs.writeFile(dataPath, arr, err => {
-  //   if(err) console.log(err);
-  // })
 });
-
--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const request = require("request");
 
 let dataPath = path.join(__dirname, "../chirps.json");
 
-
-fs.readFile(dataPath,{ 
-  encoding: "UTF-8"}, (err,data) => {
-  console.log(data);
-});
-
-
+fs.readFile(
+  dataPath,
+  {
+    encoding: "UTF-8",
+  },
+  (err, data) => {
+    console.log(data);
+  }
+);
